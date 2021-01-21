f0c084cd93dc5c069cf84187ff7c43974af6eca5
S460 <aleksandar.ta6ev@gmail.com>
2021-01-21 16:48:04 +0200

    Added a giphy depending on the weather

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import './style/style.css';
 import { createCard, switchCF } from './dom.js';
 const API_KEY = '0a4f4a6eecd2d2972049aaf3d53317b8';
+const GIPHY_API_KEY = 'CtWKWgDFEoAZdObMpNzJngDXDmiTvo4q';
 const form = document.querySelector('form');
 const searchBar = document.getElementById('searchBar');
+const giphy = document.getElementById('giphy');
+
+async function showGiphy(topic) {
+	console.log(1);
+	const response = await fetch(
+		`https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${topic}`,
+		{ mode: 'cors' }
+	);
+	try {
+		const giphyData = await response.json();
+		giphy.src = giphyData.data.images.original.url;
+	} catch (err) {
+		console.log(err.message);
+	}
+}
 
 async function getWeather(location) {
 	const response = await fetch(
@@ -15,7 +31,8 @@ async function getWeather(location) {
 		const data = await response.json();
 		createCard(data);
 		switchCF(data);
-		console.log(data);
+		showGiphy(data.weather[0].main);
+		console.log(data.weather[0].main);
 	}
 }
 
