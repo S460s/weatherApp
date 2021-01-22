900672e29c175e100bbe6c5046dfaaeca13ea74a
S460 <aleksandar.ta6ev@gmail.com>
2021-01-22 09:02:41 +0200

    Made a simple search bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './style/style.css';
 import { createCard, switchCF } from './dom.js';
-const API_KEY = '0a4f4a6eecd2d2972049aaf3d53317b8';
+const WEATHER_API_KEY = '0a4f4a6eecd2d2972049aaf3d53317b8';
 const GIPHY_API_KEY = 'CtWKWgDFEoAZdObMpNzJngDXDmiTvo4q';
 const form = document.querySelector('form');
 const searchBar = document.getElementById('searchBar');
 const giphy = document.getElementById('giphy');
 
 async function showGiphy(topic) {
-	console.log(1);
 	const response = await fetch(
 		`https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${topic}`,
 		{ mode: 'cors' }
@@ -22,17 +21,16 @@ async function showGiphy(topic) {
 
 async function getWeather(location) {
 	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=${API_KEY}&units=metric`,
+		`https://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=${WEATHER_API_KEY}&units=metric`,
 		{ mode: 'cors' }
 	);
 	if (!response.ok) {
-		console.log('Err');
+		console.log('Response was not ok.');
 	} else {
 		const data = await response.json();
 		createCard(data);
 		switchCF(data);
 		showGiphy(data.weather[0].main);
-		console.log(data.weather[0].main);
 	}
 }
 
@@ -44,5 +42,4 @@ function searchEvent() {
 	});
 }
 getWeather('Pernik');
-
 searchEvent();
